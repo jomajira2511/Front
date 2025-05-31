8baf768e368889bea2548e2b5702dd0e04224335
Jhon <54144793+jomajira2511@users.noreply.github.com>
2025-05-31 15:13:26 -0500

    Add files via upload

---
+++ CursoCsharp/OperadoresAritmeticos/FormOperadoresAritmeticos.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CursoCsharp.OperadoresAritmeticos
+{
+    partial class FormOperadoresAritmeticos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNumero1 = new System.Windows.Forms.TextBox();
+            this.TxtNumero2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.BackColor = System.Drawing.Color.Gold;
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(15, 110);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(78, 55);
+            this.btnSuma.TabIndex = 0;
+            this.btnSuma.Text = "Suma";
+            this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtNumero1
+            // 
+            this.TxtNumero1.Location = new System.Drawing.Point(91, 25);
+            this.TxtNumero1.Name = "TxtNumero1";
+            this.TxtNumero1.Size = new System.Drawing.Size(149, 20);
+            this.TxtNumero1.TabIndex = 3;
+            // 
+            // TxtNumero2
+            // 
+            this.TxtNumero2.Location = new System.Drawing.Point(91, 58);
+            this.TxtNumero2.Name = "TxtNumero2";
+            this.TxtNumero2.Size = new System.Drawing.Size(149, 20);
+            this.TxtNumero2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(99, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resultado : ";
+            // 
+            // LblResultado
+            // 
+            this.LblResultado.AutoSize = true;
+            this.LblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResultado.Location = new System.Drawing.Point(195, 132);
+            this.LblResultado.Name = "LblResultado";
+            this.LblResultado.Size = new System.Drawing.Size(15, 20);
+            this.LblResultado.TabIndex = 6;
+            this.LblResultado.Text = "-";
+            // 
+            // FormOperadoresAritmeticos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtNumero2);
+            this.Controls.Add(this.TxtNumero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSuma);
+            this.Name = "FormOperadoresAritmeticos";
+            this.Text = "FormOperadoresAritmeticos";
+            this.Load += new System.EventHandler(this.FormOperadoresAritmeticos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNumero1;
+        private System.Windows.Forms.TextBox TxtNumero2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblResultado;
+    }
+}
