1dfa1917758a1eadaa1aab69198cc9df70ae767e
Jhon <54144793+jomajira2511@users.noreply.github.com>
2025-06-15 23:51:31 -0500

    Add files via upload

---
+++ Proyectos en Visual Basic/PruebaProyecto/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BTNIngresar.Click
+        txtNombre.Text = ("HOLA MUNDO ")
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNombre.Click
+
+    End Sub
+
+End Class
