d2471b6a293f5fc3dfc3a1cb0865137cf9d6fad2
Jhon <54144793+jomajira2511@users.noreply.github.com>
2025-06-16 21:59:21 -0500

    Add files via upload

---
+++ C# desde 0/CursoCsharp/OperadoresAritmeticos/FormOperadoresAritmeticos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoCsharp.OperadoresAritmeticos
+{
+    public partial class FormOperadoresAritmeticos : Form
+    {
+        public FormOperadoresAritmeticos()
+        {
+            InitializeComponent();
+        }
+
+        int Numero1;
+        int Numero2;
+        int Resultado;
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormOperadoresAritmeticos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            Numero1 = Convert.ToInt32 (TxtNumero1.Text);
+            Numero2 = Convert.ToInt32(TxtNumero2.Text);
+            Resultado = Numero1 + Numero2;
+            LblResultado.Text = Convert.ToString(Resultado);
+
+        }
+    }
+}
